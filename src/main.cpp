25ccf57d1928a211b80feb14b19aea9f91394467
Shell <yury.udavichenka@gmail.com>
2022-10-19 17:22:43 +0200

    initial setup

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#define GLFW_INCLUDE_VULKAN
+#include "GLFW/glfw3.h"
+
+#include <iostream>
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "ERROR Failed to initialize GLFW! Aborting..." << std::endl;
+    }
+
+    if (!glfwVulkanSupported()) {
+        std::cerr << "ERROR Vulkan is not supported! Aborting..." << std::endl;
+        return -1;
+    }
+
+    return 0;
+}
