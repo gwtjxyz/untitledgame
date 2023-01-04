0674a51308f6bb53fbea30c2e0cd4ba10a34e4bd
Shell_ <goodwarrior1234@gmail.com>
2023-01-04 21:49:44 +0100

    add new stuff

---
+++ src/framework/common/constants.hpp
@@ -0,0 +1,24 @@
+//----------------------------------------------------------------------------------------
+/**
+* \file       window.hpp
+* \author     Yury Udavichenka
+* \date       05/11/2022
+* \brief      Constants
+*
+*  Header containing various constants used by the framework
+*
+*/
+//----------------------------------------------------------------------------------------
+#pragma once
+
+#include "types.hpp"
+
+namespace se {
+	static const i32 OK  = 0;
+	static const i32 ERR = -1;
+
+	// temporary
+	static const i32 WINDOW_WIDTH = 1280;
+	static const i32 WINDOW_HEIGHT = 720;
+	static const char * const WINDOW_TITLE = "Starlight Engine";
+}
---
+++ src/framework/core.hpp
@@ -0,0 +1,17 @@
+//----------------------------------------------------------------------------------------
+/**
+* \file       window.hpp
+* \author     Yury Udavichenka
+* \date       05/11/2022
+* \brief      Core header
+*
+*  Header including core functionality commonly used by the rest of the application,
+*  e. g. types, logging, constants, etc.
+*
+*/
+//----------------------------------------------------------------------------------------
+#pragma once
+
+#include "common/types.hpp"
+#include "common/logging.hpp"
+#include "common/constants.hpp"
